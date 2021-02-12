1628ad9e9c28f21ce81247885eb8c1eddf74db57
Ellenkreynolds <Ellenkreynolds@users.noreply.github.com>
2021-02-12 00:48:53 -0500

    styling

--- js/index.js
+++
@@ -1,7 +0,0 @@
-var ready = (callback) => {
-  if (document.readyState != "loading") callback();
-  else document.addEventListener("DOMContentLoaded", callback);
-}
-ready(() => {
-  document.querySelector(".fobhero").style.height = window.innerHeight + "px";
-})
